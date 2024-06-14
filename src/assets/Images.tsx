da006977eb5322360af347d5a44232dcce8e14ef
unknown <priyanshu.shethink@gmail.com>
2024-06-14 20:04:45 +0530

    add logo image

--- src/assets/Images.tsx
+++ src/assets/Images.tsx
@@ -1,4 +1,5 @@
 import signup from './signup_login_image.png'
+import my_car from './my_car.png'
 export const Images=()=>{
     signup
 }
--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -3,6 +3,7 @@ import styles from './styles.module.css'
 import SignupForm from './SignupForm'
 // import  {Images } from '@/assets/Images'
 import signup from '../../assets/signup_login_image.png'
+import logo from '../../assets/my_car.png'
 import Image from 'next/image'
 import { url } from 'inspector'
 
@@ -17,7 +18,7 @@ export default function Signup() {
                 }}></div>
         </div>
         <div className={styles.right_wrapper}>
-            <img src='' alt='logo'/>
+        <Image src={logo} alt="Logo" width={100} height={100} className="mx-auto mb-4" />
             <p className={styles.subheading}>My Car</p>
             <p className={styles.heading}>Signup Account</p>
             <SignupForm/>
