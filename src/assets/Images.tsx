01d0f943963f01938d0cdb32583d88f374f7d859
unknown <priyanshu.shethink@gmail.com>
2024-06-14 19:31:44 +0530

    add signup image

---
+++ src/assets/Images.tsx
@@ -0,0 +1,4 @@
+import signup from './signup_login_image.png'
+export const Images=()=>{
+    signup
+}
--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import styles from './styles.module.css'
 import SignupForm from './SignupForm'
+// import  {Images } from '@/assets/Images'
+import signup from '../../assets/signup_login_image.png'
+import Image from 'next/image'
+import { url } from 'inspector'
 
 export default function Signup() {
   return (
     <div className={styles.wrapper}>
         <div className={styles.left_wrapper}>
-            <img src='' alt='signup-car-image'/>
+            {/* <Image src={signup} alt='signup-car-image' className={styles.signup_img}/> */}
+            <div className='w-full h-full'  style={{
+                    backgroundImage: `url(${signup.src})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'left'
+                }}></div>
         </div>
         <div className={styles.right_wrapper}>
             <p className={styles.subheading}>My Car</p>
