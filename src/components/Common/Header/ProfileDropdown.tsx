475f98dd973f696e357c89ba399a11bbc06638b5
unknown <priyanshu.shethink@gmail.com>
2024-06-29 13:46:54 +0530

    added profile dropdown

---
+++ src/components/Common/Header/ProfileDropdown.tsx
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import { Avatar, Menu, MenuItem, Divider, ListItemIcon } from "@mui/material";
+import { Person, PrivacyTip, Settings, Logout } from "@mui/icons-material";
+import { Images } from "@/assets/Images";
+import Image from "next/image";
+
+const ProfileDropdown = ({ user }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <div onClick={handleClick} className="cursor-pointer">
+        <Image
+        
+          width={50}
+          height={50}
+          className="hidden sm:flex"
+          src={user?.profileImage || Images.demoProfile}
+          alt="profile"
+        />
+        <Image
+          className="sm:hidden"
+          src={Images.userProfile}
+          alt="Profile"
+          width={20}
+          height={20}
+        />
+      </div>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          elevation: 1,
+          sx: {
+            overflow: "visible",
+            mt: 1.5,
+            "& .MuiMenuItem-root": {
+                fontFamily: "rajdhani",
+                fontWeight: "500",
+                fontSize: "18px",
+                color: "black",
+                letterSpacing: "0.5px",
+              
+              padding: "4px 25px",
+              margin: "4px",
+              minHeight: "35px",
+              "&:hover": {
+                backgroundColor: "rgba(55, 65, 81, 0.1)" // Tailwind bg-gray-600
+              }
+            },
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "& .MuiDivider-root": {
+              margin: "4px 10px"
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem  onClick={handleClose} >
+          <ListItemIcon>
+            <Image
+              src={Images.viewProfile}
+              alt="profile"
+              width={20}
+              height={20}
+            />
+          </ListItemIcon>
+          View Profile
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Image
+              src={Images.privacyPolicy}
+              alt="profile"
+              width={22}
+              height={22}
+            />
+          </ListItemIcon>
+          Privacy Policy
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Image src={Images.setting} alt="profile" width={20} height={20} />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Image src={Images.logout} alt="profile" width={15} height={15} />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default ProfileDropdown;
--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -1,40 +1,37 @@
-
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 import Image from "next/image";
 import { Images } from "@/assets/Images";
 import Button from "../Button";
 import { getLocalStorage } from "@/constants/constants";
+import ProfileDropdown from "./ProfileDropdown";
 
 function Header() {
-  
-      const [user, setUser] = useState(null)  
-  useEffect(() =>{
-      setUser(getLocalStorage('user'))
-  })
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    setUser(getLocalStorage("user"));
+  }, []);
+
   return (
     <div className={`container_space large_layout ${styles.wrapper}`}>
       <div>
         <Image src={Images.logo} alt="logo" width={65} height={65} />
       </div>
 
+      {/* Desktop View */}
       <div className={styles.right_part}>
-        <Button otherStyles="bg-secondary text-white upercase sm:px-6 sm:h-fit sm:py-3">
+        <Button otherStyles="bg-secondary text-white uppercase sm:px-6 sm:h-fit sm:py-3">
           <Image src={Images.subscription} alt="subscription" width={24} height={24} />
-          <p>SUBSCRIBTION</p>
+          <p>SUBSCRIPTION</p>
         </Button>
 
         <Image src={Images.menu} alt="menu" width={25} height={25} className="cursor-pointer" />
-        <Image src={Images.notification} alt="notification" width={25} height={25} className="cursor-pointer"/>
+        <Image src={Images.notification} alt="notification" width={25} height={25} className="cursor-pointer" />
+
         <div className="flex items-center gap-[14px]">
-          <div
-            className="w-[60px] h-[60px] rounded-[50%] bg-gray-400
-          "
-          >
-            <Image src={user?.profileImage || Images.demoProfile } alt="profile image"/>
-             
-          </div>
+          <ProfileDropdown user={user} />
           <div>
             <p className={styles.name}>Austin Robertson</p>
             <p className={styles.role}>Admin</p>
@@ -42,13 +39,13 @@ function Header() {
         </div>
       </div>
 
+      {/* Mobile View */}
       <div className={styles.responsive_right_part}>
         <div className={styles.responsive_subscription}>
-        <Image src={Images.subscription} alt="subscription" width={20} height={20} />
-
+          <Image src={Images.subscription} alt="subscription" width={20} height={20} />
         </div>
         <div className={styles.responsive_profile}>
-        <Image src={Images.userProfile} alt="subscription" width={20} height={20} />
+          <ProfileDropdown user={user} />
         </div>
       </div>
     </div>
