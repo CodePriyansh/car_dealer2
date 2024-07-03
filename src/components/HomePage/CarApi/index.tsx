a206f313d90dd9a249b6256f9a1db8ab5e565a24
Pragati-patel <peehupatel1998@gmail.com>
2024-07-03 22:48:03 +0530

    chane base url

--- src/components/HomePage/CarApi/index.tsx
+++ src/components/HomePage/CarApi/index.tsx
@@ -4,6 +4,8 @@ import Cookies from "universal-cookie";
 
 const CarApi = ({ selectedOptions, initial, setCars }) => {
   const [payload, setPayload] = useState({});
+  const cookies = new Cookies();
+  let token = cookies.get("authToken");
 
   useEffect(() => {
     console.log(selectedOptions);
@@ -24,14 +26,13 @@ const CarApi = ({ selectedOptions, initial, setCars }) => {
   }, [selectedOptions, initial]);
 
   const ApplyFilterApiCall = async () => {
-    const cookies = new Cookies();
-    let token = cookies.get("token");
-    console.log(payload);
+    console.log(token,"token")
+    console.log(payload,"payload");
     try {
       const response = await instance.post("/api/cars/all", payload, {
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2ODQ1Mjc1NjlkODJmNTdlNDIwZGI2YyIsInBob25lTnVtYmVyIjoiODk2NTk4NDM5OCIsImZpcmViYXNlVXNlcklkIjoiODRCRzUyWjhIa1ptU2ZmWDZjRjNDb1FRaTRtMiIsInByb2ZpbGVJbWFnZSI6Imh0dHBzOi8vZmlyZWJhc2VzdG9yYWdlLmdvb2dsZWFwaXMuY29tL3YwL2IvbXktY2FyLTc2NTU0LmFwcHNwb3QuY29tL28vcHJvZmlsZV9pbWFnZXMlMkYxNzE5OTQ3ODkyMTc4X3RoJTIwKDE5KS5qcGc_YWx0PW1lZGlhJnRva2VuPTI5YThhZjRlLTk4Y2EtNDZiMy1hMjNhLTIyNGE1ZTdjYmUxMyIsIm5hbWUiOiJib3JpbmdjYW52YXNUb2tlbiIsImlhdCI6MTcxOTk0Nzg5MywiZXhwIjoxNzIwMDM0MjkzfQ.xT7vGNyPOKGvLVw2eRDAh6EMojoylNJbPQLD2y8IFn8`,
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2N2M1ZmMzYWNjYzRhMGQyNTJmMDUxZiIsInBob25lTnVtYmVyIjoiMTIzNDU2Nzg5MCIsImZpcmViYXNlVXNlcklkIjoiVTNxeGxYdFFHaGduSHJTSWtzRmdJSnU5WXJ6MiIsImlhdCI6MTcyMDAxODE2MSwiZXhwIjoxNzIwMTA0NTYxfQ.kVFeHWLNv1WScEkmqunsJCqKEjiFzmcjs68c2APDnMU`,
         },
       });
       setCars(response?.data?.data);
